5e192291d158b703da0a3b5dd2cc8525b89c527e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 16:01:34 +0000

    Challenge 1 complete

---
+++ JavascriptPreCourse/FirstCodeAlong/js.js
@@ -0,0 +1,18 @@
+//Challenge No 1
+function HandlePositiveNegativeNumber(event) {
+
+    let EnteredNumber = event.target.value;
+
+    if (EnteredNumber >= 0) {
+        alert("Positive");
+    }
+    else {
+        alert("Negative")
+    }
+
+
+
+
+
+
+}
