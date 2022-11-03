b2eafac217ebed1fdfabc111331e5e826688f83f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 12:51:13 +0000

    JS Calculator added

--- JavascriptPreCourse/FirstCodeAlong/javascript.js
+++ JavascriptPreCourse/FirstCodeAlong/javascript.js
@@ -1,7 +1,35 @@
-function CalculateSomething(event) {
-    const CurrentAge = event.target.value;
+function HandleCalculateGrades(event) {
 
+    let grade = event.target.value;
 
-    alert(CurrentAge)
-    
+    if (!grade) {
+        return;
+    }
+
+    grade = grade.toUpperCase();
+
+    let Percentage;
+    switch (grade) {
+        case "A":
+            Percentage = "90-100%";
+            break;
+        case "B":
+            Percentage = "75-90%";
+            break;
+        case "C":
+            Percentage = "50-75%";
+            break;
+        case "D":
+            Percentage = "25-50%";
+            break;
+        case "E":
+            Percentage = "0-25%";
+            break;
+        case "F":
+            Percentage = "0%";
+            break;
+    }
+
+alert("The percentage was " + Percentage);
+  
 }
